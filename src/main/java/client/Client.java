ae09e794b5c607ea162a05b38c7b1e3ca07e6928
Alexis PINEAU <E18A737Q@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-10-09 16:28:13 +0200

    Updated Client interface and implementation

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,25 +1,9 @@
 package client;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Client extends UnicastRemoteObject implements ClientInterface {
 
 
-public class Client {
-
-    private Client() {}
-
-    public static void main(String[] args) {
-
-        String host = (args.length < 1) ? null : args[0];
-        try {
-            Registry registry = LocateRegistry.getRegistry(host);
-            Hello stub = (Hello) registry.lookup("Hello");
-            String response = stub.sayHello();
-            System.out.println("response: " + response);
-        } catch (Exception e) {
-            System.err.println("Client exception: " + e.toString());
-            e.printStackTrace();
-        }
-    }
 
 }
--- src/main/java/client/ClientInterface.java
+++ src/main/java/client/ClientInterface.java
@@ -1,4 +1,13 @@
 package client;
 
-public interface ClientInterface {
+import java.rmi.Remote;
+import java.util.ArrayList;
+
+public interface ClientInterface extends Remote {
+
+    public void setNextPlayer(Player player);
+    public void setPreviousPlayer(Player player);
+    public void dealCards(ArrayList<Card> cards);
+    public Card showCard();
+
 }
