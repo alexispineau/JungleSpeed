d0ab74dffa84b17001f6a851f44af5ac01d77143
Alexis Pineau <alexis@paradise-city.lan>
2019-10-10 11:13:37 +0200

    Added methods in Client classes

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,9 +1,39 @@
 package client;
 
+import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Stack;
 
 public class Client extends UnicastRemoteObject implements ClientInterface {
 
+    private Player nextPlayer;
+    private Player previousPlayer;
+    private Stack<Card> hand;
+    private Card card;
 
+    public Client() throws RemoteException {
+
+    }
+
+    public void setNextPlayer(Player nextPlayer) {
+        this.nextPlayer = nextPlayer;
+    }
+
+    public void setPreviousPlayer(Player player) {
+        this.previousPlayer = previousPlayer;
+    }
+
+    public void setHand(Stack<Card> hand) {
+        this.hand = (Stack<Card>)hand.clone();
+    }
+
+    public Card getCard() {
+        return this.card;
+    }
+
+    public void turnCard() {
+        this.card = this.hand.pop();
+        //TODO Notify other players
+    }
 
 }
--- src/main/java/client/ClientInterface.java
+++ src/main/java/client/ClientInterface.java
@@ -1,13 +1,14 @@
 package client;
 
 import java.rmi.Remote;
-import java.util.ArrayList;
+import java.util.Stack;
 
 public interface ClientInterface extends Remote {
 
     public void setNextPlayer(Player player);
     public void setPreviousPlayer(Player player);
-    public void dealCards(ArrayList<Card> cards);
-    public Card showCard();
+    public void setHand(Stack<Card> hand);
+    public Card getCard();
+    public void turnCard();
 
 }
