25ccc384392c43daa450797848df2de6e1b3e9c9
Alexis Pineau <alexis@paradise-city.lan>
2019-10-15 16:37:45 +0200

    G fait quelques petites modifs sur Client

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -6,28 +6,43 @@ import java.util.Stack;
 
 public class Client extends UnicastRemoteObject implements ClientInterface {
 
-    private Player nextPlayer;
-    private Player previousPlayer;
+    private ClientInterface nextPlayer;
+    private ClientInterface previousPlayer;
     private Stack<Card> playerStack;
     private Stack<Card> discardStack;
     private Card card;
 
     public Client() throws RemoteException { }
 
-    public void setNextPlayer(Player nextPlayer) {
+    public void setNextPlayer(ClientInterface nextPlayer) {
         this.nextPlayer = nextPlayer;
     }
 
-    public void setPreviousPlayer(Player previousPlayer) {
+    public void setPreviousPlayer(ClientInterface previousPlayer) {
         this.previousPlayer = previousPlayer;
+    }
+
+    public Card getBottomCardOfNextPlayer() {
+        //TODO
+        return null;
+    }
+
+    public int getNbCardOfNextPlayer() {
+        //TODO
+        return -1;
+    }
+
+    public Card getBottomCard() {
+        return this.card;
+    }
+
+    public int getMyNbcard() {
+        //TODO
+        return -1;
     }
 
     public void setHand(Stack<Card> hand) {
         this.playerStack = (Stack<Card>)hand.clone();
-    }
-
-    public Card getCard() {
-        return this.card;
     }
 
     public void turnCard() {
--- src/main/java/client/ClientInterface.java
+++ src/main/java/client/ClientInterface.java
@@ -1,18 +1,17 @@
 package client;
 
 import java.rmi.Remote;
-import java.util.ArrayList;
+import java.util.Stack;
 
 public interface ClientInterface extends Remote {
 
     public void setNextPlayer(ClientInterface player);
+    // Moi je pense qu'il faut supprimer cette méthode^^
+    public void setPreviousPlayer(ClientInterface player);
     public Card getBottomCardOfNextPlayer();
     public int getNbCardOfNextPlayer();
-    
-    public void setPreviousPlayer(ClientInterface player);
-    
     public Card getBottomCard();
     public int getMyNbcard();
-    public void setHand(ArrayList<Card> crds);
+    public void setHand(Stack<Card> crds);
 
 }
