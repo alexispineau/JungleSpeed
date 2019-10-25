92339bb97aabdb21ccaaf7fc35d7f0300a4f7a60
LouisQ56 <1questel.louis@gmail.com>
2019-10-25 17:20:08 +0200

    lantement mais surrement

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -2,13 +2,16 @@ package client;
 
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
+import server.Server;
 import server.ServerInterface;
 
 public class Client extends UnicastRemoteObject implements ClientInterface {
 
+	private ServerInterface server;
     private ClientInterface nextPlayer;
     private ClientInterface previousPlayer;
     private ArrayList<Card> playerStack;
@@ -67,6 +70,31 @@ public class Client extends UnicastRemoteObject implements ClientInterface {
     }
 
 	public ArrayList<Card> getPlayerStack() {return this.discardStack;}
+	
+	// Envoie un signal au serveur pour signifier que je veux jouer
+	public void IWantPlay(int localPort) {
+		try {
+			// connection au serveur
+            server = (ServerInterface) Naming.lookup("//127.0.0.1:8090/Server");
+            System.out.println("Interface server récuppérée");
+            System.out.println(server.test());
+            
+            // mise à disposition de l'interface du client
+            String name = "//127.0.0.1:"+localPort+"/Client";
+            LocateRegistry.createRegistry(localPort);
+            ClientInterface client = this;
+            ClientInterface stub =
+                    (ClientInterface) UnicastRemoteObject.exportObject(client, 0);
+            Naming.rebind(name,stub);
+            System.out.println("Client lancé");
+            
+            //Appel à la méthode joinGame du serveur pour commencer une partie
+            server.joinGame(client);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
 	
 	public static void main(String[] args) {
         try {
